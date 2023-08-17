68a5c9f65d7378ea95adc3ca83228072da6a9032
Hendrik Valcke <hendrik.valcke@student.uantwerpen.be>
2023-08-17 18:10:58 +0200

    track active devices

--- src/observer.c
+++ src/observer.c
@@ -10,62 +10,120 @@
 #include <zephyr/bluetooth/hci.h>
 #include <zephyr/kernel.h>  // Include this header for k_uptime_get()
 
+#define MAX_DEVICES 10  // Maximum number of unique devices to track
 #define NAME_LEN 30
-#define MAX_DEVICES 15  // Maximum number of unique devices to track
+#define INACTIVITY_TIMEOUT_MS (15 * 1000)  // 15s in milliseconds
 
+struct active_device {
+    bt_addr_le_t addr;
+    uint32_t last_seen;
+};
 
-static bt_addr_le_t scanned_devices[MAX_DEVICES];  // Array to store scanned addresses
-static int num_scanned_devices = 0;  // Counter for the number of scanned devices
+static struct active_device active_devices[MAX_DEVICES];  // Array to store active devices
+static int num_active_devices = 0;  // Counter for the number of active devices
 
-// Check if a device address has already been scanned
-static bool is_device_scanned(const bt_addr_le_t *addr)
+// Check if a device address is already tracked as active
+static struct active_device *find_active_device(const bt_addr_le_t *addr)
 {
-    for (int i = 0; i < num_scanned_devices; i++) {
-        if (bt_addr_le_cmp(addr, &scanned_devices[i]) == 0) {
-            return true;
+    for (int i = 0; i < num_active_devices; i++) {
+        if (bt_addr_le_cmp(addr, &active_devices[i].addr) == 0) {
+            return &active_devices[i];
         }
     }
-    return false;
+    return NULL;
 }
-// Add a device address to the list of scanned devices
-static void add_scanned_device(const bt_addr_le_t *addr)
+
+// Add a device address to the list of active devices or update the timestamp
+static void update_active_device(const bt_addr_le_t *addr, const char *addr_str)
 {
-    if (num_scanned_devices < MAX_DEVICES) {
-        scanned_devices[num_scanned_devices++] = *addr;
+    struct active_device *device = find_active_device(addr);
+    uint32_t timestamp = k_uptime_get_32();  // Get uptime in milliseconds
+
+    if (device) {
+        device->last_seen = timestamp;
+    } else if (num_active_devices < MAX_DEVICES) {
+        active_devices[num_active_devices].addr = *addr;
+        active_devices[num_active_devices].last_seen = timestamp;
+
+        // Format the timestamp as hh:mm:ss
+        uint32_t seconds = timestamp / 1000;
+        uint32_t minutes = seconds / 60;
+        uint32_t hours = minutes / 60;
+
+        seconds %= 60;
+        minutes %= 60;
+
+        // Format the timestamp as hh:mm:ss
+        char timestamp_str[9];  // "hh:mm:ss\0"
+        snprintf(timestamp_str, sizeof(timestamp_str), "%02u:%02u:%02u", hours, minutes, seconds);
+
+        printk("New active device detected: %s, Timestamp: %s, Current active devices: %d\n", addr_str, timestamp_str,num_active_devices + 1);
+        num_active_devices++;
     }
 }
-static void device_found(const bt_addr_le_t *addr, int8_t rssi, uint8_t type,
-			 struct net_buf_simple *ad)
-{
-	/*type: BT_GAP_ADV_TYPE_ 	0 means Scannable and connectable advertising.
-								1 means Directed connectable advertising.
-								2 means Non-connectable and scannable advertising.
-								3 means Non-connectable and non-scannable advertising.
-								4 means Additional advertising data requested by an active scanner.
-								5 means Extended advertising, see advertising properties.
-	*/
-	char addr_str[BT_ADDR_LE_STR_LEN];
-	uint32_t uptime_ms = k_uptime_get_32();  // Get uptime in milliseconds
-	
-	bt_addr_le_to_str(addr, addr_str, sizeof(addr_str));
-	// Convert milliseconds to hours, minutes, seconds
-    uint32_t seconds = uptime_ms / 1000;
-    uint32_t minutes = seconds / 60;
-    uint32_t hours = minutes / 60;
-    seconds %= 60;
-    minutes %= 60;
-	// Format the timestamp as hh:mm:ss
-    char timestamp_str[9];  // "hh:mm:ss\0"
-    snprintf(timestamp_str, sizeof(timestamp_str), "%02u:%02u:%02u", hours, minutes, seconds);
 
+// Remove inactive devices based on inactivity timeout
+static void remove_inactive_devices(const char *addr_str)
+{
+    uint32_t current_time = k_uptime_get_32();
+
+    for (int i = 0; i < num_active_devices; ) {
+        if (current_time - active_devices[i].last_seen > INACTIVITY_TIMEOUT_MS) {
+            // Format the timestamp as hh:mm:ss
+            uint32_t seconds = active_devices[i].last_seen / 1000;
+            uint32_t minutes = seconds / 60;
+            uint32_t hours = minutes / 60;
+
+            seconds %= 60;
+            minutes %= 60;
+
+            // Format the timestamp as hh:mm:ss
+            char timestamp_str[9];  // "hh:mm:ss\0"
+            snprintf(timestamp_str, sizeof(timestamp_str), "%02u:%02u:%02u", hours, minutes, seconds);
+
+            printk("Inactive device removed: %s, Last Seen: %s, Current active devices: %d\n", addr_str, timestamp_str,num_active_devices - 1 );
+            num_active_devices--;
+
+            // Remove inactive device by shifting remaining devices
+            for (int j = i; j < num_active_devices; j++) {
+                active_devices[j] = active_devices[j + 1];
+            }
+        } else {
+            i++;
+        }
+    }
+}
+
+/*bl addr types: BT_GAP_ADV_TYPE_ 	0 means Scannable and connectable advertising.
+										1 means Directed connectable advertising.
+										2 means Non-connectable and scannable advertising.
+										3 means Non-connectable and non-scannable advertising.
+										4 means Additional advertising data requested by an active scanner.
+										5 means Extended advertising, see advertising properties.
+	*/
+static void device_found(const bt_addr_le_t *addr, int8_t rssi, uint8_t type,
+                         struct net_buf_simple *ad)
+{
+    char addr_str[BT_ADDR_LE_STR_LEN];
+
+    bt_addr_le_to_str(addr, addr_str, sizeof(addr_str));
+
+    update_active_device(addr, addr_str);
 	
-	if (!is_device_scanned(addr)) {
+	
+	//printk("Device found: %s (RSSI %d), type %u, AD data len %u\n",addr_str, rssi, type, ad->len);
+
+    remove_inactive_devices(addr_str);
+	
+	
+	/*if (!is_device_scanned(addr)) {
         add_scanned_device(addr);
         printk("New device found: %s (RSSI %d), type %u, AD data len %u, Timestamp: %s  \n",
                addr_str, rssi, type, ad->len, timestamp_str);
     } else {
-        //printk("Duplicate device found: %s (RSSI %d), type %u, AD data len %u\n", addr_str, rssi, type, ad->len);
+        printk("Duplicate device found: %s (RSSI %d), type %u, AD data len %u\n", addr_str, rssi, type, ad->len);
     }
+	*/
 }
 
 #if defined(CONFIG_BT_EXT_ADV)
